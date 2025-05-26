4e5c5f1f86c004b9df7334c8750984b3d37822b6
Virgilio Sirqueira Cruvinel <virgiliocomp@gmail.com>
2025-05-26 19:25:34 -0300

    Fix on Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        char gender;
+        int beerQuantity;
+        int barbecueQuantity;
+        int softDrinkQuantity;
+
+        System.out.print("Sexo: ");
+        gender = scanner.next().charAt(0);
+        System.out.print("Quantidade de cervejas: ");
+        beerQuantity = scanner.nextInt();
+        System.out.print("Quantidade de refrigerantes: ");
+        softDrinkQuantity = scanner.nextInt();
+        System.out.print("Quantidade de espetinhos: ");
+        barbecueQuantity = scanner.nextInt();
+
+        Bill bill = new Bill(gender, beerQuantity, barbecueQuantity, softDrinkQuantity);
+        System.out.println(bill);
     }
 }
